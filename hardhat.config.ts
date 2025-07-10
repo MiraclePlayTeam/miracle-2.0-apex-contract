973f4db2d3f4f6340f4784972564a2e22924941d
realwater <pie1104@nate.com>
2025-07-10 12:09:28 +0900

    1.0.0 토너먼트 컨트랙트 new 버전 추가

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,11 +1,19 @@
-import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "dotenv/config";
+import { HardhatUserConfig } from "hardhat/config";
 
 const sourcePath = process.env.SOURCE_PATH || "./contract";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.28",
+  solidity: {
+    version: "0.8.23",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 100
+      }
+    }
+  },
   paths: {
     sources: sourcePath,
     artifacts: "./artifacts",
