95f9b93562435afca9e90419b92252c21c12e22a
dave <dave@sevenlinelabs.com>
2025-07-14 16:28:04 +0900

    chore: modify pragma version

--- contract/Miracle-Governance/Miracle-Governance.sol
+++ contract/Miracle-Governance/Miracle-Governance.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.26;
+pragma solidity ^0.8.23;
 
 import "@thirdweb-dev/contracts/extension/PermissionsEnumerable.sol";
 import "@thirdweb-dev/contracts/extension/ContractMetadata.sol";
